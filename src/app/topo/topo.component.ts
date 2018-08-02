9d3b94e21fd30a2fb68a0dbac305f10847f90fef
Felipe Pliger <felipe.pliger@ciesglobal.org>
2018-08-02 09:10:42 -0300

    atualizando pesquisa de ofertas

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,29 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { OfertasService } from '../ofertas.service';
+import { Subject } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-topo',
   templateUrl: './topo.component.html',
   styleUrls: ['./topo.component.css'],
-  providers: [ OfertasService ]
+  providers: [OfertasService]
 })
 export class TopoComponent implements OnInit {
+
+  ofertas;
+  private subjectPesquisa: Subject<string> = new Subject<string>()
 
   constructor(
     private ofertasService: OfertasService
   ) { }
 
   ngOnInit() {
-
+    this.ofertas = this.subjectPesquisa.pipe(
+      switchMap((busca: string) => {
+        console.log('busca');
+        return this.ofertasService.pesquisaOfertas(busca);
+      })
+    );
+    this.ofertas.subscribe(ofertas => {
+      console.log(ofertas.json());
+      
+    })
   }
 
-  pesquisa(busca){
-    this.ofertasService.pesquisaOfertas(busca).subscribe(response => {
-      console.log(response.json());
-      
-    },(error) => {
-      console.log(error);
-     });
+  pesquisa(busca) {
+    this.subjectPesquisa.next(busca);
   }
 
 }
