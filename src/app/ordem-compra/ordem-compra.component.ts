b2275e2da3225e331c0201f4a8b1267c95436179
Felipe Pliger <felipe.pliger@ciesglobal.org>
2018-08-09 09:05:01 -0300

    começando form de ordem compra

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  endereco = null;
+  numero = null;
+  complemento = null;
+  formaPagamento = null;
+
+  enderecoValido = false;
+  numeroValido = false;
+  complementoValido = false;
+  formaPagamentoValido = false;
+
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  atualizaEndereco(endereco) {
+    this.endereco = endereco;
+    console.log(this.endereco);
+  }
+
+  atualizaNumero(numero) {
+    this.numero = numero;
+    console.log(this.numero);
+  }
+
+  atualizaComplemento(complemento) {
+    this.complemento = complemento;
+    console.log(this.complemento);
+  }
+
+  atualizaFormaPagamento(formaPagamento) {
+    this.formaPagamento = formaPagamento;
+    console.log(this.formaPagamento);
   }
 
 }
